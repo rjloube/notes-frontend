e766fad9045e6cc734255903209af5f847c1e43b
russell-loube-optimizely <russell.loube@optimizely.com>
2024-08-29 15:05:07 -0500

    added css id selector for note input

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -25,6 +25,7 @@ const NoteForm = ({ createNote }) => {
           value={newNote}
           onChange={handleChange}
           placeholder="write note content here"
+          id="note-input"
         />
         <button type="submit">save</button>
       </form>
